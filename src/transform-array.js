1376031f2f51894a73e689edeeb862dd886b6585
Lena Zamnius <lena.zamnius@gmail.com>
2020-02-25 16:10:47 +0200

    feat:upload solution of transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,34 +1,24 @@
 module.exports = function transform (arr) {
-	// if (!Array.isArray(arr)) throw new TypeError();
-	// if (!arr.some((val) => typeof val === 'string' && val.slice(0, 3) === '--d') || arr.length === 0) {
-	// 	return arr;
-	// }
-	// else {
-	// 	console.log(arr);
-	// 	const newArr = [];
-	// 	for (let i = 0; i > arr.length; i++) {
-	// 		if (typeof arr[i] === 'string' && arr[i].slice(0, 3) === '--d') {
-	// 			switch (arr[i]) {
-	// 				case '--discard-next':
-	// 					arr.splice(i + 1, 1);
-	// 					break;
-	// 				case '--discard-prev':
-	// 					newArr.splice(i - 1, 1);
-	// 					break;
-	// 				case '--double-next':
-	// 					if (arr[i + 1]) newArr.push(arr[i + 1]);
-	// 					break;
-	// 				case '--double-prev':
-	// 					if (arr[i - 1]) newArr.push(arr[i - 1]);
-	// 					break;
-	// 			}
-	// 		}
-	// 		else {
-	// 			newArr.push(arr[i]);
-	// 		}
-	// 	}
-	// 	console.log(newArr);
-	// 	return newArr;
-	// }
-	throw 'Not implemented';
+	if (!Array.isArray(arr)) throw new TypeError();
+	if (!arr.some((val) => typeof val === 'string' && val.slice(0, 3) === '--d') || arr.length === 0) {
+		return arr;
+	} else {
+		const newArr = [];
+		for(let i = 0; i < arr.length; i++) {
+			if(typeof arr[i] === 'string' && arr[i].slice(0, 3) === '--d') {
+				if(arr[i] === '--discard-next') {
+					i++;
+				} else if(arr[i] === '--discard-prev') {
+					newArr.splice(-1);
+				} else if(arr[i] === '--double-next') {
+					if((i+1) < arr.length) newArr.push(arr[i + 1]);
+				} else if(arr[i] === '--double-prev') {
+					if(i > 0) newArr.push(arr[i - 1]);
+				}
+			} else {
+				newArr.push(arr[i]);
+			}
+		}
+		return newArr;
+	}
 };

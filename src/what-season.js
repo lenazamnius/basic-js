1b753141f84afc44558846026210e3bea02ed4b4
Lena Zamnius <lena.zamnius@gmail.com>
2020-02-26 12:32:09 +0200

    feat: upload solution of what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(!date) return 'Unable to determine the time of year!';
+  if(!(Object.prototype.toString.call(date) === '[object Date]')) throw new Error();
+  console.log(typeof date)
+  let month = date.getMonth();
+  if(month === 2 || month === 3 || month === 4) {
+    return 'spring';
+  } else if(month === 5 || month === 6 || month === 7) {
+    return 'summer';
+  } else if(month === 8 || month === 9 || month === 10) {
+    return 'autumn';
+  } else if(month === 11 || month === 0 || month === 1) {
+    return 'winter';
+  }
 };
